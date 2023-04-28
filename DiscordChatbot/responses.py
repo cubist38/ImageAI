9378d213554e4024bb6d5c2503150a2064f4d0a2
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-04-28 13:10:41 +0700

    a

--- DiscordChatbot/responses.py
+++
@@ -1,6 +0,0 @@
-def get_response(message: str) -> str:
-
-    if message.isdigit():
-        return str(int(message) + 1)
-    
-    return 'I didn\'t understand what you wrote. Try typing "!help".'
