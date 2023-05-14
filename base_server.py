ad5a0858caedaa2b569e4be07ef43ee8ac760bf0
Huy Vuong <cubist38@gmail.com>
2023-05-14 16:18:45 +0700

    a

--- base_server.py
+++ base_server.py
@@ -4,13 +4,7 @@ import json
 
 with open('./config.json', 'r') as f:
     config = json.load(f)
-ngrok.set_auth_token(config['ngrok_token']'])
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World!"
+ngrok.set_auth_token(config['ngrok_token'])
 
 if __name__ == '__main__':
     # Start ngrok and create a tunnel for the Flask app
