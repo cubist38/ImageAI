4486d24d0d86e32ac15339fdaa43e096617c748f
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-14 20:21:35 +0700

    a

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
 import streamlit as st
 from PIL import Image
-from st_clickable_images import clickable_images
-import io
-import base64
 
-# Define a function to convert the uploaded image to a byte stream
-def image_to_byte_array(image):
-    img_byte_arr = io.BytesIO()
-    image.save(img_byte_arr, format='JPEG')
-    img_byte_arr = img_byte_arr.getvalue()
-    return img_byte_arr
-
-def bytestream_to_base64(bytestream):
-    return base64.b64encode(img_byte_arr).decode()
-
-# Define the on_click callback function
-def on_click(x, y):
-    st.write("Clicked at", x, y)
 
 # Display a file uploader to upload an image
 uploaded_file = st.file_uploader("Upload an image here", type=["jpg", "jpeg", "png"])
@@ -26,12 +10,14 @@ uploaded_file = st.file_uploader("Upload an image here", type=["jpg", "jpeg", "p
 if uploaded_file is not None:
     # Load the image
     image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
     
-    # Convert the image to a byte stream
-    img_byte_arr = image_to_byte_array(image)
-    img_base64 = bytestream_to_base64(img_byte_arr)
+    # Get user click position
+    click_pos = st.session_state.get('click_pos')
+    if click_pos is not None:
+        st.write("You clicked at:", click_pos)
 
-    # Display the clickable image
-    st.image(img_base64, use_column_width=True, caption="Click me!")
-    if st.button("Click me!"):
-        on_click(0, 0)
+    # Add click handler to image
+    if st.button("Click on the image"):
+        click_pos = st.mouse_clicks.add(click_data=image)
+        st.session_state['click_pos'] = click_pos
