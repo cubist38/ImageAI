cf39383822d066bbdb5834212b8a2bf5f7f0720f
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-14 21:48:26 +0700

    a

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from streamlit_image_coordinates import streamlit_image_coordinates
 from PIL import Image, ImageDraw
 import numpy as np
+import torch
+from InpaintAnything.utils import dilate_mask, show_mask
+from sam_segment import predict_masks_with_sam
+
 
 RADIUS = 5
 
@@ -20,19 +24,30 @@ def resize_with_aspect_ratio(image, max_width = 640):
     return image.resize((new_width, new_height))
 
 def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     st.title("Image Cropper Demo")
     image_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
     if image_file is not None:
         image = Image.open(image_file)
         image = resize_with_aspect_ratio(image)
-        if img.mode == "RGBA":
-            img = img.convert("RGB")
-        image =  np.array(image)
         coords = streamlit_image_coordinates(image)
-        
         if coords:
-            st.write(coords)
-            st.image(draw_point_on_image(image, (int(coords["x"]), int(coords["y"])), RADIUS), caption="Selected point", use_column_width=True)
+            if image.mode == "RGBA":
+                image = image.convert("RGB")
+            image =  np.array(image)
+            masks, scores, logits = predict_masks_with_sam(
+                image,
+                [(int(coords["x"]), int(coords["y"]))],
+                1,
+                model_type= "vit_h",
+                ckpt_p = "/content/drive/MyDrive/InpaintAnything/Weights/sam_vit_h_4b8939.pth",
+                device = device,
+            )
+            masks = masks.astype(np.uint8) * 255
+            masks = [dilate_mask(mask, 15) for mask in masks]
+            # find mask with highest score
+            mask = masks[np.argmax(scores)]
+            st.image(mask)
 
 if __name__ == "__main__":
     main()
