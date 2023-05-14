2a5f7be28e6351d13b54bc87d1ae7fb52fe0a4a7
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-14 20:42:58 +0700

    a

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st
-from streamlit_cropper import st_cropper, draw_cropped_image
+from streamlit_cropper import st_cropper
+from PIL import Image, ImageDraw
 
+def draw_point_on_image(image, coords):
+    img = Image.fromarray(image)
+    draw = ImageDraw.Draw(img)
+    draw.point((coords[0], coords[1]), fill="red")
+    return img
 
 def main():
     st.title("Image Cropper Demo")
     image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
     if image:
         cropped_image, coords = st_cropper(image=image, realtime_update=True, return_coords=True)
-        st.image(draw_cropped_image(cropped_image, coords), caption="Cropped Image")
+        st.image(draw_point_on_image(cropped_image, coords), caption="Cropped Image")
         st.write(f"Clicked at: {coords}")
 
 if __name__ == "__main__":
