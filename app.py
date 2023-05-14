9275b9e5442b0b718cf8bacbd24b71a50773b9c6
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-14 21:07:53 +0700

    a

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def main():
     image_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
     if image_file is not None:
         image = Image.open(image_file)
-        st.image(image, caption='Uploaded Image.')
-        coords = streamlit_image_coordinates(image)
-        if coords:
-            st.write("Coordinates: ", coords)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+        # coords = streamlit_image_coordinates(image)
+        # if coords:
+        #     st.write("Coordinates: ", coords)
 
 if __name__ == "__main__":
     main()
