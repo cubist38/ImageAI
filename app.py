2b5a0e350f9a063babcccad915bdc56971110afb
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-14 19:29:14 +0700

    a

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
-    # Do something with the image, e.g. display it
-    st.image(uploaded_file)
+    # Load the image and create a canvas
+    image = Image.open(uploaded_file)
+    canvas = st_canvas(
+        width=image.width,
+        height=image.height,
+        image_mode='RGB'
+    )
+
+    # Display the image and wait for the user to click
+    st.image(image)
+    if canvas.clicked:
+        x, y = canvas.coordinates
+        st.write(f"You clicked at ({x}, {y})")
