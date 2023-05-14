f7cadbca5841800d2d9aba05253907155695a978
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-14 21:35:26 +0700

    a

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_image_coordinates import streamlit_image_coordinates
 from PIL import Image, ImageDraw
+import numpy as np
 
 RADIUS = 5
 
@@ -24,7 +25,11 @@ def main():
     if image_file is not None:
         image = Image.open(image_file)
         image = resize_with_aspect_ratio(image)
+        if img.mode == "RGBA":
+            img = img.convert("RGB")
+        image =  np.array(image)
         coords = streamlit_image_coordinates(image)
+        
         if coords:
             st.write(coords)
             st.image(draw_point_on_image(image, (int(coords["x"]), int(coords["y"])), RADIUS), caption="Selected point", use_column_width=True)
