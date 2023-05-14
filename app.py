69afe5798f6600d892fbb1858d34c9da35967f14
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-14 19:36:28 +0700

    a

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
+from streamlit_drawable_canvas import st_canvas
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
