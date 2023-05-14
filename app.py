af4db65e01b6dee50326cc99b762daec06f5cb8d
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-14 19:57:52 +0700

    a

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 import streamlit as st
-import numpy as np
 from PIL import Image
+from st_clickable_images import clickable_images
+
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+def on_image_click(x, y):
+    st.write("You clicked on the image at coordinates:", x, y)
+
 if uploaded_file is not None:
     # Load the image and create a canvas
     image = Image.open(uploaded_file)
     # Create a canvas element
     st.image(image)
+    st_clickable_image = st_clickable_image(image, on_image_click)
 
-# Create a canvas element
-canvas = st.empty()
+    # Display the clickable image
+    if st_clickable_image is not None:
+        st.write(st_clickable_image)
 
-# Add an event listener to the canvas element to detect when the user clicks on it
-def on_click(event):
-  x = event.x
-  y = event.y
-
-  # Draw a point at the coordinates of the click
-  canvas.fill_rect((x - 5, y - 5, 10, 10), "red")
-
-canvas.on_change(on_click)
-
-# # Display the image with the clickable images component
-# clicked_image = clickable_images([image], canvas=canvas)
