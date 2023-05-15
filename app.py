46c51042c305ce7117cf867ba784322d21756190
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-15 17:15:43 +0700

    a

--- app.py
+++ app.py
@@ -9,20 +9,20 @@ from streamlit_image_coordinates import streamlit_image_coordinates as st_image_
 RADIUS = 5
 
 def main():
-    if st.button("Remove Anything"):
-        st.write("Click on an object in the image, and Inpainting Anything will remove it instantly!")
-        image_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-        if image_file is not None:
-            image = Image.open(image_file)
-            image = resize_with_aspect_ratio(image, 700)
-            coords = st_image_coordinates(image)
-            if coords:
-                st.write("Coordinates: ", coords)
-                st.image(draw_point_on_image(image, (int(coords["x"]), int(coords["y"])), radius = RADIUS), use_column_width=True)  
-                remove_button = create_center_button(name = "Remove")  
-                if remove_button:
-                    img_inpainted = remove_on_clicked(image, coords)
-                    st.image(img_inpainted, use_column_width=True)
+    # if st.button("Remove Anything"):
+    st.write("Click on an object in the image, and Inpainting Anything will remove it instantly!")
+    image_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if image_file is not None:
+        image = Image.open(image_file)
+        image = resize_with_aspect_ratio(image, 700)
+        coords = st_image_coordinates(image)
+        if coords:
+            st.write("Coordinates: ", coords)
+            st.image(draw_point_on_image(image, (int(coords["x"]), int(coords["y"])), radius = RADIUS), use_column_width=True)  
+            remove_button = create_center_button(name = "Remove")  
+            if remove_button:
+                img_inpainted = remove_on_clicked(image, coords)
+                st.image(img_inpainted, use_column_width=True)
 
 if __name__ == "__main__":
     main()
