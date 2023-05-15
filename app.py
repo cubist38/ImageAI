bd3755d9b309acc2b19f12831b7884f41fcd5eee
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-15 17:30:15 +0700

    a

--- app.py
+++ app.py
@@ -11,23 +11,22 @@ RADIUS = 5
 
 def main():
     if selected_feature:
-        #feature = st.radio('Choose an event', features, index=0)
         feature = st.selectbox('Choose a feature', features)
         st.write('Selected Feature:', feature)
         if feature == 'Remove Anything Image':
             st.write("Click on an object in the image, and Inpainting Anything will remove it instantly!")
             image_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-            if image_file is not None:
-                image = Image.open(image_file)
-                image = resize_with_aspect_ratio(image, 700)
-                coords = st_image_coordinates(image)
-                if coords:
-                    st.write("Coordinates: ", coords)
-                    st.image(draw_point_on_image(image, (int(coords["x"]), int(coords["y"])), radius = RADIUS), use_column_width=True)  
-                    remove_button = create_center_button(name = "Remove")  
-                    if remove_button:
-                        img_inpainted = remove_on_clicked(image, coords)
-                        st.image(img_inpainted, use_column_width=True)
+            # if image_file is not None:
+            image = Image.open(image_file)
+            image = resize_with_aspect_ratio(image, 700)
+            coords = st_image_coordinates(image)
+            if coords:
+                st.write("Coordinates: ", coords)
+                st.image(draw_point_on_image(image, (int(coords["x"]), int(coords["y"])), radius = RADIUS), use_column_width=True)  
+                remove_button = create_center_button(name = "Remove")  
+                if remove_button:
+                    img_inpainted = remove_on_clicked(image, coords)
+                    st.image(img_inpainted, use_column_width=True)
         elif feature == 'Remove Anything Video':
             st.write("With a single click on an object in the first video frame, Remove Anything Video can remove the object from the whole video!")
 if __name__ == "__main__":
