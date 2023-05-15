87245e7fe4778c4852d2df5b0225584856dab951
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-15 17:02:51 +0700

    a

--- remove.py
+++ remove.py
@@ -4,7 +4,7 @@ from utils import dilate_mask
 from lama_model import load_lama_model, inpaint_img_with_builded_lama
 from PIL import Image
 from sam_model import load_sam_model, predict_masks_with_sam
-from utils import draw_point_on_image, create_center_button
+from engine import draw_point_on_image, create_center_button
 import numpy as np
 import torch
 
