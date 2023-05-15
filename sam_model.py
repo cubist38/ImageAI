895b3cc6e209a972bbb8e3d3ef18531bd85a6f2b
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-15 17:20:48 +0700

    a

--- sam_model.py
+++ sam_model.py
@@ -15,17 +15,9 @@ def predict_masks_with_sam(
         img: np.ndarray,
         point_coords: List[List[float]],
         point_labels: List[int],
-        model_type: str,
-        ckpt_p: str,
-        device="cuda"
-):
+        predictor: SamPredictor):
     point_coords = np.array(point_coords)
     point_labels = np.array(point_labels)
-    sam = sam_model_registry[model_type](checkpoint=ckpt_p)
-    sam.to(device=device)
-    predictor = SamPredictor(sam)
-
-    predictor.set_image(img)
     masks, scores, logits = predictor.predict(
         point_coords=point_coords,
         point_labels=point_labels,
