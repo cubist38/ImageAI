84432fe6b55af466f08f2962f67a68ef6f3450a4
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-15 22:45:52 +0700

    a

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def main():
         st.markdown("## With a single click on an object in the first video frame, our technique can remove the object from the whole video!")
         video_file = st.file_uploader("Upload a video", type=["mp4", "mov"])
         if video_file is not None:
-            vidcap = cv2.VideoCapture(video_file)
+            video_bytes = video_file.read()
+            vidcap = cv2.VideoCapture(video_bytes)
             images = [] 
             while True:
                 success,image = vidcap.read()
