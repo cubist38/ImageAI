77d3f9a4eeb50c58ec242a256ab588dea0874589
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-16 16:03:46 +0700

    a

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from engine import (resize_pil_keep_aspect_ratio, resize_rgb_keep_aspect_ratio,
                     draw_point_on_image, create_center_button, write_bytesio_to_file)
 from streamlit_image_coordinates import streamlit_image_coordinates as st_image_coordinates
 import cv2
-import os
+import tempfile
 
 features = ['Remove Anything Image', 'Remove Anything Video', 'Replace Anything']
 RADIUS = 5
@@ -31,18 +31,18 @@ def main():
         st.markdown("## With a single click on an object in the first video frame, our technique can remove the object from the whole video!")
         video_file = st.file_uploader("Upload a video", type=["mp4", "mov", "gif"])
         if video_file is not None:
-            temp_file_to_save = './temp_file_1.mp4'
-            write_bytesio_to_file(temp_file_to_save, video_file)
-            vidcap = cv2.VideoCapture(temp_file_to_save)   
-            os.remove(temp_file_to_save)
+            st.video(video_file)
+            tfile = tempfile.NamedTemporaryFile(delete=False)
+            tfile.write(video_file.read())
+            vidcap = cv2.VideoCapture(tfile.name)   
             fps = vidcap.get(cv2.CAP_PROP_FPS)
             frames = [] 
             while True:
                 success, frame = vidcap.read()
                 if not success:
                     break
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                frame = resize_rgb_keep_aspect_ratio(frame, 640)
+                #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                #frame = resize_rgb_keep_aspect_ratio(frame, 640)
                 frames.append(frame)
             first_frame = Image.fromarray(frames[0])
             coords = st_image_coordinates(first_frame)
