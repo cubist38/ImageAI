d063107b1f737e67b8aa0655d1fa7d1a3fbc027e
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-25 21:39:47 +0700

    a

--- remove_anything/engine.py
+++ remove_anything/engine.py
@@ -31,7 +31,7 @@ def dilate_mask(mask, dilate_factor=15):
 
 def create_center_button(name: dict, image_data = None):
     # Apply CSS to center the button
-    col1, col2, col3 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
     with col1:
         download = st.download_button(name["download"], data=image_data, file_name='image_with_mask.png', mime='image/png')
     with col2 :
