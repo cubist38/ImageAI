83aec509a691e6c970c8b6f2f498feb97ebc4eb4
Huy Vuong <cubist38@gmail.com>
2023-05-31 22:57:44 +0700

    'a'

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-if uploaded_file is not None:
-    # Do something with the image, e.g. display it
-    st.image(uploaded_file)
