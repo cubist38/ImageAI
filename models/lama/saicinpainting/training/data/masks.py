6246271b661df7d51494e3d8f96e3b5d8c37dd4f
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-07-05 22:32:13 +0700

    a

--- models/lama/saicinpainting/training/data/masks.py
+++ models/lama/saicinpainting/training/data/masks.py
@@ -7,8 +7,8 @@ from enum import Enum
 import cv2
 import numpy as np
 
-from saicinpainting.evaluation.masks.mask import SegmentationMask
-from saicinpainting.utils import LinearRamp
+from models.lama.saicinpainting.evaluation.masks.mask import SegmentationMask
+from models.lama.saicinpainting.utils import LinearRamp
 
 LOGGER = logging.getLogger(__name__)
 
