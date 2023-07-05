8fc1b2b962f789b71aa3be06f8cd5bc16a266bb6
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-07-05 22:38:34 +0700

    a

--- app/features/gen_image.py
+++ app/features/gen_image.py
@@ -1,9 +1,9 @@
 from app.features.config import get_settings
-from app.helpers.load_diffusion import load_diffusion_model
+from app.helpers.load_diffusion import load_diffusion
 
 config = get_settings()
 
 def gen_image_from_prompt(prompt: str, num_inference_step = 30):
-    pipeline = load_diffusion_model(config.gen_image_model_name, config.device)
+    pipeline = load_diffusion(config.gen_image_model_name, config.device)
     image = pipeline(prompt, num_inference_step = num_inference_step)
     return image
