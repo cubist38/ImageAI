d619c7e68f6ba550c2511120246ebc7e0724c871
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-07-07 23:21:27 +0700

    Change cfonig

--- client/image-ai/src/api/config.js
+++ client/image-ai/src/api/config.js
@@ -1,2 +1,2 @@
 export const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
-export const apiServer = "https://774d-35-184-137-53.ngrok-free.app";
+export const apiServer = "https://5524-35-184-137-53.ngrok-free.app";
