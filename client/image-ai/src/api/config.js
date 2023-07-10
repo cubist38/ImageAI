d163169ba764b9630d310c73b2685fa929a7fa36
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-07-10 22:31:47 +0700

    modify code ui

--- client/image-ai/src/api/config.js
+++ client/image-ai/src/api/config.js
@@ -1,2 +1,2 @@
 export const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
-export const apiServer = "https://b5d5-35-223-92-109.ngrok-free.app";
+export const apiServer = "https://200e-34-142-176-126.ngrok-free.app";
