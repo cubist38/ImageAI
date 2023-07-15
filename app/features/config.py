6dc88286f444ff7196149f507f9c2759ad5fe00b
Ho Van Son <88260562+sonhv3112@users.noreply.github.com>
2023-07-15 16:50:14 +0700

    fix weights path

--- app/features/config.py
+++ app/features/config.py
@@ -5,9 +5,9 @@ import torch
 class Settings(BaseSettings):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     sam_model_type: str = "vit_h"
-    sam_model_ckpt_p: str = "/content/drive/MyDrive/InpaintAnything/Weights/sam_vit_h_4b8939.pth"
+    sam_model_ckpt_p: str = "./Weights/sam_vit_h_4b8939.pth"
     lama_model_config_p: str = "./models/lama/configs/prediction/default.yaml"
-    lama_model_ckpt_p: str = "/content/drive/MyDrive/InpaintAnything/Weights/big-lama"
+    lama_model_ckpt_p: str = "./Weights/big-lama"
     caption_model_name: str = "blip-large"
     clip_model_name: str = "ViT-L-14/openai"
     gen_image_model_name: str = "runwayml/stable-diffusion-v1-5"
